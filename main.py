91229dcdc4d542ca3fdf853ce47978cb2a0e99c0
eugene liyai <eugeneliyai@eugenes-MacBook-Pro.local>
2019-04-08 14:20:37 +0300

    feat: check ratings

--- main.py
+++ main.py
@@ -8,16 +8,14 @@ bookFile = 'data/BX-Books.csv'
 data = pd.read_csv(dataFile, sep=';', header=0, names=['user', 'isbn', 'rating'])
 books = pd.read_csv(bookFile, sep=';', header=0, error_bad_lines=False, usecols=[0,1,2], index_col=0, names=['isbn', 'title', 'author'])
 
-print(data.head())
-print(books.head())
+# print(data.head())
+# print(books.head())
+#
+# print(book_meta('0002005018', books))
+# print(fav_books(204622, 5, data, books))
 
-print(book_meta('0002005018', books))
+print(data.shape) # number of rows and columns
 
-# def fav_books(user, N):
-#     newdata = data[data['isbn'].isin(books.index)]
-#     userRatings = newdata[data['user'] == user]
-#     sortedRatings = pd.DataFrame.sort_values(userRatings, ['rating'], ascending=[0])[:N]
-#     sortedRatings['title'] = sortedRatings['isbn'].apply(book_meta, args=(books,))
-#     return sortedRatings
+user_per_isbn = data.isbn.value_counts()
 
-print(fav_books(204622, 5, data, books))
+data = data[data['isbn'].isin(user_per_isbn[user_per_isbn > 10].index)]
